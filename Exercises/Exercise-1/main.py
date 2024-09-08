08ccc21cde4afac66eac3e0ebc61ebe388ef9366
Jaemin Kweon <jmkweon88@gmail.com>
2024-09-08 12:10:49 +0900

    Trying out ThreadPoolExecutor and checking runtime

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -3,6 +3,8 @@ import os
 import zipfile
 import asyncio
 import aiohttp
+import time
+from concurrent.futures import ThreadPoolExecutor
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -14,31 +16,22 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
-
-def main():
-    # your code here
-    x = os.getcwd()+"\\Exercises\\Exercise-1"
-    if not os.path.exists(rf"{x}\downloads"):
-        os.mkdir(rf"{x}\downloads")
-    os.chdir(rf"{x}\downloads")
-    for i in download_uris:
-        valid = False
-        fn = i.split('/')[len(i.split('/')) - 1]
-        sc = requests.head(i, allow_redirects=True).status_code
-        if  sc == 200:
-            print(f'{fn} status code 200. Now downloading.')
-            resp = requests.get(i)
-            with open(fn, 'wb') as file:
-                file.write(resp.content)
-            print(f'{fn} download complete. Now unzipping.')
-            with zipfile.ZipFile(fn,'r') as zf:
-                zf.extractall()
-            print(f'{fn} unzipped. Now deleting zip file.')
-            os.remove(fn)
-        else:
-            print(f'{fn} status code: {sc}')
-            pass
-    pass
+def series_dl(url):
+    #Download loop function
+    fn = url.split('/')[len(url.split('/')) - 1]
+    sc = requests.head(url, allow_redirects=True).status_code
+    if sc == 200:
+        print(f'{fn} status code 200. Now downloading.')
+        resp = requests.get(url)
+        with open(fn, 'wb') as file:
+            file.write(resp.content)
+        print(f'{fn} download complete. Now unzipping.')
+        with zipfile.ZipFile(fn,'r') as zf:
+            zf.extractall()
+        print(f'{fn} unzipped. Now deleting zip file.')
+        os.remove(fn)
+    else:
+        print(f'{fn} status code: {sc}')
 
 async def amain():
     #asyncio + aiohttp version
@@ -63,6 +56,23 @@ async def amain():
                     print(f'{fn} status code: {aresp.status}')
                     pass
 
+def main():
+    # your code here
+    x = os.getcwd()+"\\Exercises\\Exercise-1"
+    if not os.path.exists(rf"{x}\downloads"):
+        os.mkdir(rf"{x}\downloads")
+    os.chdir(rf"{x}\downloads")
+    for i in download_uris:
+        series_dl(i)
+    #with ThreadPoolExecutor(max_workers=4) as tpe:
+        #tpe.map(series_dl,download_uris)
+
 if __name__ == "__main__":
+    start_time = time.time()
     #main()
     asyncio.run(amain())
+    print(f"Total completion time: {time.time() - start_time} secs")
+
+#Series Runtime: 155.87 secs
+#Async Runtime with no concurrency: 163.98 secs
+#TPE Runtime: 80.29 secs
